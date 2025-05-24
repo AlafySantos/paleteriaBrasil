6031954a4057bb949aad40a3b80cda0863509719
jonathas <jonathaspereira139@gmail.com>
2025-05-24 07:45:54 -0300

    DTO fornecedor completo

--- src/main/java/engSoft/paleteriaBrasil/DTO/FornecedorRequestDTO.java
+++ src/main/java/engSoft/paleteriaBrasil/DTO/FornecedorRequestDTO.java
@@ -1,8 +1,20 @@
 package engSoft.paleteriaBrasil.DTO;
 
-public record FornecedorRequestDTO(Integer idFornecedor) {
+public record FornecedorRequestDTO(Integer idFornecedor, String telefone, String cnpj, String nomeFornecedor) {
     public Integer getIdFornecedor() {
         return this.idFornecedor;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public String getCnpj() {
+        return this.cnpj;
+    }
+
+    public String getNomeFornecedor() {
+        return this.nomeFornecedor;
     }
 }
 
