948f0779aa78fd5719cd0d6e7da9d7cc7a77d296
jonathas <jonathaspereira139@gmail.com>
2025-05-24 08:22:44 -0300

    POM.xml atualizado (maven estava sem config

--- src/main/java/engSoft/paleteriaBrasil/controllers/FornecedorController.java
+++ src/main/java/engSoft/paleteriaBrasil/controllers/FornecedorController.java
@@ -1,18 +1,13 @@
 package engSoft.paleteriaBrasil.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import engSoft.paleteriaBrasil.entities.Fornecedor;
-import engSoft.paleteriaBrasil.repositories.FornecedorRepository;
 import engSoft.paleteriaBrasil.services.FornecedorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.AllArgsConstructor;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/fornecedor")
